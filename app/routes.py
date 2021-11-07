699cd020a72c0d26e01a47a7e77b8d9226f1b8d4
Serguei Yanush <syanush@gmail.com>
2021-11-07 18:15:03 +0300

    Add template for home page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    user = {'username': 'Lola'}
+    return render_template('index.html', title='Home', user=user)
