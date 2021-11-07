9225ee2e5030ae81af1982a0292ebf42eec6d9de
Serguei Yanush <syanush@gmail.com>
2021-11-08 00:51:06 +0300

    Using endpoint names instead of urls

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
+def index():
     user = {'username': 'Lola'}
     books = [
         {
@@ -26,5 +26,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'
               .format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign in', form=form)
