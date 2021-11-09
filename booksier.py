8d6e54179f0389e036a89016a72a93d4a72b4be9
Serguei Yanush <syanush@gmail.com>
2021-11-09 23:14:54 +0300

    Configure flask shell

--- booksier.py
+++ booksier.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.user import User
+from app.book import Book
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Book': Book}
