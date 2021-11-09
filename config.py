41fed72a55526acf872f0cd69b8e1dad3f78993a
Serguei Yanush <syanush@gmail.com>
2021-11-09 14:27:53 +0300

    Add database model for User entity

--- config.py
+++ config.py
@@ -3,6 +3,15 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     # class variables
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
