8f5761b2f94b0a39c1b43f6e5cf9ec4335f80932
Serguei Yanush <syanush@gmail.com>
2021-11-11 00:24:27 +0300

    Sending errors by email

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -18,3 +20,24 @@ from app.registration_form import RegistrationForm
 from app.book_form import BookForm
 from app.edit_profile_form import EditProfileForm
 from app import routes, errors
+
+
+def send_mail():
+    if app.config['MAIL_SERVER']:
+        auth = None
+    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    secure = None
+    if app.config['MAIL_USE_TLS']:
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'], subject='Booksier Failure',
+        credentials=auth, secure=secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+
+if not app.debug:
+    send_mail()
